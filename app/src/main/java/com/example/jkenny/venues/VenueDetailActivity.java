fef344e693dc44290b9b950b2425e99602f932c4
John Kenny <jkenny@gilt.com>
2015-05-26 16:12:35 +0100

    Add Picasso images and Hugo debugging

--- app/src/main/java/com/example/jkenny/venues/VenueDetailActivity.java
+++ app/src/main/java/com/example/jkenny/venues/VenueDetailActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,8 +12,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import hugo.weaving.DebugLog;
 
 
 public class VenueDetailActivity extends Activity {
@@ -31,9 +35,49 @@ public class VenueDetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue_detail);
         ButterKnife.inject(this);
+        initImage();
         initButtons();
     }
 
+    @DebugLog
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @DebugLog
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @DebugLog
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @DebugLog
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @DebugLog
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @DebugLog
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    private void initImage() {
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(image);
+    }
     private void initButtons() {
         openInBrowser.setOnClickListener(new View.OnClickListener() {
             @Override
